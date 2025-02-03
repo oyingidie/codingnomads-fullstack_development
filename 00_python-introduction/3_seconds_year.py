3b76bc6522b5117a6148a39cff7cc591f8951561
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-03 16:52:18 +0100

    Around the globe

--- 00_python-introduction/3_seconds_year.py
+++ 00_python-introduction/3_seconds_year.py
@@ -2,11 +2,16 @@
 # 1. Then write the code in this script file below this line.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# sixty seconds in a minute
 seconds_minute = 60
+# sixty minutes in an hour
 minutes_hour = 60
+# twenty-four hours in a day
 hours_day = 24
+# three hundred and sixty-five days in a year
 days_year = 365
 
+# multiply all for the number of seconds in a year
 seconds_year = seconds_minute * minutes_hour * hours_day * days_year
 
 print(seconds_year)
