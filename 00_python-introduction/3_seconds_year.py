ad6795e1587ea4bcf7f9d2e8ef781911ce123e1f
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-03 14:47:42 +0100

    Around the globe

---
+++ 00_python-introduction/3_seconds_year.py
@@ -0,0 +1,12 @@
+# 0. Use the interpreter to calculate how many seconds are in a year.
+# 1. Then write the code in this script file below this line.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+seconds_minute = 60
+minutes_hour = 60
+hours_day = 24
+days_year = 365
+
+seconds_year = seconds_minute * minutes_hour * hours_day * days_year
+
+print(seconds_year)
