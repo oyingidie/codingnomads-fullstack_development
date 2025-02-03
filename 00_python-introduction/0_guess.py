652cf27625584d308cf1ef4d3311dab58f71dfb7
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-03 12:01:51 +0100

    This ain't no game

--- 00_python-introduction/0_guess.py
+++ 00_python-introduction/0_guess.py
@@ -1,13 +1,24 @@
 # programming concepts in the guess-my-number game
 
+# - getting a package (code written by someone else)
 import random
 
+# - variable and assignment
+# - using a function from a package
 number = random.randint(1, 30)
 guess = None
 
+# - while loop
+# - comparison operator
 while guess != number:
+    # - indentation
+    # - collecting user input
+    # - type conversion
     guess = int(input("Guess a number between 1 and 30: "))
     
+    # - conditional statement
+    # - control statement
+    # - terminal output
     if guess == number:
         print("Congratulations! You guessed correctly and won!")
         break
