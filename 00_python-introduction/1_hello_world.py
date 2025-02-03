becf7e1808bbf3c7ea51e7a0e4a2b9504c56a2ff
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-03 13:22:19 +0100

    Python says "hello..."

--- 00_python-introduction/1_hello_world.py
+++ 00_python-introduction/1_hello_world.py
@@ -1,4 +1,5 @@
 # 0. Below the comments, write code in this script that prints out "hello world".
 # 1. Use your Python interpreter, to also print "hello world!".
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 print("hello world")
