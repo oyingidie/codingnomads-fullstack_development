6848c1198af05fb462011b256be2ba47dd8c8243
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-04 22:31:59 +0100

    Lego from get-go

---
+++ 00_python-introduction/8_program_pseudocode.py
@@ -0,0 +1,22 @@
+# SCENARIO: Think about a task that you want to accomplish using programming
+# Maybe you want to automate something you do every day on your computer?
+#
+# 0. Break down some of the steps you can think of and write them in
+#    this file as pseudocode.
+# 1. You can come back and update this file when you've learned more.
+# 2. You can also add real Python code after learning the necessary concepts.
+# 3. For now, just practice breaking larger tasks into smaller steps
+#    and writing out your thoughts in pseudocode.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# open the browser
+# go to the website
+# input username
+# enter the password
+# Login
+# click on "Utilities" module
+# navigate to the "Financial Interface Transactions" page
+# go to home page
+# Logout
+# close the browser
+# end
