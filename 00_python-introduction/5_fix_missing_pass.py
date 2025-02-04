46a86340800cb58c24891de131c3003f09428d0b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-04 09:14:26 +0100

    Your friendly neighbourhood placeholder

---
+++ 00_python-introduction/5_fix_missing_pass.py
@@ -0,0 +1,11 @@
+# SCENARIO: The code below runs into an error.
+#
+# 0. Fix it, without making Python do anything.
+# +++++++++++++++++++++++++++++++++++++++++++++
+
+flag = False
+while flag:
+    # keep doing what you're doing
+    pass
+
+print("all done.")
