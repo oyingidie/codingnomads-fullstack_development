0b0e2fe264091b12b35d044eef78d0ffab35e744
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-04 08:50:40 +0100

    Snake in the grass?

---
+++ 00_python-introduction/4_pseudocode.py
@@ -0,0 +1,19 @@
+# SCENARIO: Your cat just had kittens! Now you want to put up an ad to give them
+# to your friends. You'll need to save all names of the kittens,
+# confirm that each of them is cute, and show a message that
+# that kitten is ready for adoption.
+#
+# 0. Break this task up into a couple of steps of pseudocode
+#    and write the pseudocode below in code comments.
+# 1. You don't need to write any functional code, just map out the steps.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# create a list of kitten names
+# go through each name in the list
+#   check if kitten is cute
+#   if kitten is cute, 
+#       display "Kitten is ready for adoption."
+#   otherwise,
+#       display "Kitten is not ready for adoption."
+#   repeat until all kittens are checked
+# end
