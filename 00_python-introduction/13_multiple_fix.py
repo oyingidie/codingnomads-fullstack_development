e7ad5f242cbff79a12f925a71765b38145f57938
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-05 10:13:34 +0100

    There goes your fine line

---
+++ 00_python-introduction/13_multiple_fix.py
@@ -0,0 +1,10 @@
+# 0. Fix the variable assignment shown below.
+# 1. Can you see why using the multiple variable assignment can be tricky?
+#    Declared like this, it's easy to mix which is which.
+#
+# code >>> dreams, profession = "flying", "programming"
+#
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+dreams = "flying"
+profession = "programming"
