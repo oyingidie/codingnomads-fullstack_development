d171ca54d1a191269d2768c34f1c4c611f0d3dc1
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-05 09:53:11 +0100

    First of all... Readability.

---
+++ 00_python-introduction/12_multiple_improve.py
@@ -0,0 +1,10 @@
+# 0. Improve the variable assignment shown below.
+# 1. Why should you change it?
+#
+# code >>> color, year = "red", 1900
+#
+# +++++++++++++++++++++++++++++++++++++++++++++++
+
+# better readable and easier to understand code
+color = "red"
+year = 1900
