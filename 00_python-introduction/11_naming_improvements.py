fe6439b2fa4d1995a5650e78076da48d7afbff6c
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-05 09:41:57 +0100

    A public servant's dilemma

---
+++ 00_python-introduction/11_naming_improvements.py
@@ -0,0 +1,17 @@
+# SCENARIO: The code below works as expected and there are no errors.
+# However, the variable names aren't great and don't follow the guidelines.
+#
+# code >>>  Name = "Ada"
+#           lastName = "Lovelace"
+#           FULLNAME = Name + " " + lastName
+#
+#           print(FULLNAME) >>>
+#
+# 0. Improve the variable names so they follow the PEP-8 guidelines.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+name = "Ada"
+last_name = "Lovelace"
+full_name = name + " " + last_name
+
+print(full_name)
