47f3fc476a389cce5e0a5f930ff657b92a684049
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-05 09:08:05 +0100

    Rules, Conventions, & Basic Vocabulary

---
+++ 00_python-introduction/10_naming_errors.py
@@ -0,0 +1,11 @@
+# SCENARIO: When you run the code below, you'll get SyntaxErrors.
+# The variable names are all messed up and don't follow the rules!
+#
+# code >>>  1stvariable = 40
+#           second variable = 2
+#
+#           finally = 1stvariable + second variable
+#           print(finally) >>>
+#
+# 0. Fix the variable names so the code works as expected.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
