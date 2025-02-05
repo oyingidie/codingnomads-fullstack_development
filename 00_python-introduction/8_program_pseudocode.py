2e60b14ad75cde62755869c21cd0c9a39fbf521d
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-05 05:37:47 +0100

    Lego from get-go

--- 00_python-introduction/8_program_pseudocode.py
+++ 00_python-introduction/8_program_pseudocode.py
@@ -1,11 +1,10 @@
-# SCENARIO: Think about a task that you want to accomplish using programming
-# Maybe you want to automate something you do every day on your computer?
-#
-# 0. Break down some of the steps you can think of and write them in
+# 0. Think about a task that you want to accomplish using programming
+#    e.g. automating a daily task on your computer?
+# 1. Break down some of the steps you can think of and write them in
 #    this file as pseudocode.
-# 1. You can come back and update this file when you've learned more.
-# 2. You can also add real Python code after learning the necessary concepts.
-# 3. For now, just practice breaking larger tasks into smaller steps
+# 2. You can come back and update this file when you've learned more.
+# 3. You can also add real Python code after learning the necessary concepts.
+# 4. For now, just practice breaking larger tasks into smaller steps
 #    and writing out your thoughts in pseudocode.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
