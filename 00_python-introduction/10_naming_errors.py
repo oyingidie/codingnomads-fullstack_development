0b07cdd8b2312f454606c97575353c0f597430d0
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-05 09:16:25 +0100

    Rules, Conventions, & Basic Vocabulary

--- 00_python-introduction/10_naming_errors.py
+++ 00_python-introduction/10_naming_errors.py
@@ -9,3 +9,9 @@
 #
 # 0. Fix the variable names so the code works as expected.
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+first_variable = 40
+second_variable = 2
+
+result = first_variable + second_variable
+print(result)
