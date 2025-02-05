c8d7260582f6575e940358f319c08a05b88c9601
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-05 12:43:30 +0100

    If it walks and quacks, it's Duck!

---
+++ 00_python-introduction/14_whats_my_name.py
@@ -0,0 +1,10 @@
+# 0. What data type is the variable `mystery` at the end of the script?
+# 1. What data types does it hold at certain points earlier in the script?
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+mystery = None # type: NoneType
+mystery = "Sommerfeld" # type: string
+mystery = 137 # type: integer
+mystery = mystery + 0.0 # type: float
+
+print(type(mystery))
