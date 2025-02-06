66948c5e7d9f22f50bf09d1444ac2a7ab88666ff
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-07 00:00:00 +0100

    Strange coincidence?

--- 00_python-introduction/18_area_perimeter.py
+++ 00_python-introduction/18_area_perimeter.py
@@ -1,3 +1,14 @@
 # 0. Write code to display the area and perimeter of
 #    a rectangle that has a width of 2.4 and a height of 6.4.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+width = 2.4
+height = 6.4
+
+# Area of a rectangle = width * height
+area = width * height
+# Perimeter of a rectangle = 2 * (width + height)
+perimeter = 2 * (width + height)
+
+print("Area of the rectangle: ", area)
+print("Perimeter of the rectangle: ", perimeter)
