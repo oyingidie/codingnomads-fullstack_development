c467ea0a9d0ce0b68e857e1bd3e64c7e97b26d9c
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-07 00:10:44 +0100

    Label Everything Thoroughly

---
+++ 00_python-introduction/19_cylinder.py
@@ -0,0 +1,4 @@
+# 0. Write the necessary code to calculate the volume and surface area
+#    of a cylinder with a radius of 3.14 and a height of 5.
+# 1. Print out the result.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
