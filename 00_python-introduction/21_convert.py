6f1bc4bede5395781c4de26e1b3288f11165fc55
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-07 05:53:49 +0100

    Text on arrival

---
+++ 00_python-introduction/21_convert.py
@@ -0,0 +1,10 @@
+# 0. Demonstrate how to:
+# 
+#       - convert an int to a float
+#       - convert a float to an int
+#       - perform division using a float and an int
+#       - use two variables to perform a multiplication
+#
+# 1. What information is lost during which conversions?
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
