93227e819d996d2d88846f8fbdc9876cb6a4e5e5
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-07 05:54:46 +0100

    Text on arrival

--- 00_python-introduction/21_convert.py
+++ 00_python-introduction/21_convert.py
@@ -8,3 +8,17 @@
 # 1. What information is lost during which conversions?
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# convert an int to a float
+int_to_float = float(5)
+
+# convert a float to an int
+float_to_int = int(5.0)
+
+# perform division using a float and an int
+division = 5.0 / 2
+
+# use two variables to perform a multiplication
+a = 5
+b = 2
+multiplication = a * b
+
