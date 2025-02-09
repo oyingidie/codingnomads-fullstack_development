22d9d74b29c33b14fac601481fb367d7324a98f5
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-09 13:02:10 +0100

    Errors have honest-to-good intentions

---
+++ 00_python-introduction/26_new_friends_1.py
@@ -0,0 +1,6 @@
+# 0. Write code that produces a SyntaxError when you run this script.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# invalid variable name
+3rror = "SyntaxError"
+print(3rror)
