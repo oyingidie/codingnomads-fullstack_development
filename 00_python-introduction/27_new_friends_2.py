6d3b90f06d09284430e2a689ae2bfe392f22386e
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-09 13:08:59 +0100

    Big on communication

---
+++ 00_python-introduction/27_new_friends_2.py
@@ -0,0 +1,5 @@
+# 0. Write code that produces a TypeError when you run this script.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# multiply string by non-integer
+print("TypeError" * 0.5)
