2a37714aca46ce5c51e69d622217432583c4ec77
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-09 04:42:21 +0100

    Snakes are not known for their footwear

--- 00_python-introduction/23_big_python.py
+++ 00_python-introduction/23_big_python.py
@@ -1,13 +1,13 @@
 # 0. Write the necessary code to print the BIG PYTHON string shown below.
 # 1. Research multi-line strings to make this easier for you :)
 #
-#     PPPP   Y     Y  TTTTTTTTT  H    H      O     N       N
-#     P   P   Y   Y       T      H    H     O O    N N     N
-#     P   P    Y Y        T      H    H    O   O   N  N    N
-#     PPPP      Y         T      HHHHHH    O   O   N   N   N
-#     P         Y         T      H    H    O   O   N    N  N
-#     P         Y         T      H    H     O O    N     N N
-#     P         Y         T      H    H      O     N       N
+#       PPPP   Y     Y  TTTTTTTTT  H    H      O     N       N
+#       P   P   Y   Y       T      H    H     O O    N N     N
+#       P   P    Y Y        T      H    H    O   O   N  N    N
+#       PPPP      Y         T      HHHHHH    O   O   N   N   N
+#       P         Y         T      H    H    O   O   N    N  N
+#       P         Y         T      H    H     O O    N     N N
+#       P         Y         T      H    H      O     N       N
 #
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -22,8 +22,6 @@ P         Y         T      H    H     O O    N     N N
 P         Y         T      H    H      O     N       N
 """
 
-print(big_python_quoted)
-
 # using parentheses and escape characters
 big_python_escaped = (
     "PPPP   Y     Y  TTTTTTTTT  H    H      O     N       N\n"
@@ -35,4 +33,18 @@ big_python_escaped = (
     "P         Y         T      H    H      O     N       N\n"
 )
 
+# using the join method
+big_python_joined = "\n".join([
+    "PPPP   Y     Y  TTTTTTTTT  H    H      O     N       N",
+    "P   P   Y   Y       T      H    H     O O    N N     N",
+    "P   P    Y Y        T      H    H    O   O   N  N    N",
+    "PPPP      Y         T      HHHHHH    O   O   N   N   N",
+    "P         Y         T      H    H    O   O   N    N  N",
+    "P         Y         T      H    H     O O    N     N N",
+    "P         Y         T      H    H      O     N       N",
+])
+
+# same output for all ˜\_(ツ)_/˜
+print(big_python_quoted)
 print(big_python_escaped)
+print(big_python_joined)
