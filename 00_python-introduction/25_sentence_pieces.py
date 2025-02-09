a1f96931b477532ad9f106bc7fbe1c9b7e12c05d
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-09 05:11:54 +0100

    All fingers crossed!

---
+++ 00_python-introduction/25_sentence_pieces.py
@@ -0,0 +1,17 @@
+# 0. Create a proper sentence by concatenating the strings below.
+#
+#       - q1 = "By seeking"
+#       - q2 = "and"
+#       - q3 = "blundering"
+#       - q4 = "we learn."
+#
+# 1. Add your own strings, if necessary, to make it readable.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+q1 = "By seeking"
+q2 = "and"
+q3 = "blundering"
+q4 = "we learn."
+
+sentence = q1 + " " + q2 + " " + q3 + "," + q4
+print(sentence)
