f5d9f689406a9287d3585603493629cf29e20ae0
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-10 21:03:19 +0100

    European floor numbering

---
+++ 00_python-introduction/28_pick_your_letters.py
@@ -0,0 +1,12 @@
+# 0. Use string indexing and string concatenation to write the sentence
+#    "we see trees" only by picking the necessary letters from the given string.
+#               - "tweezers "
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+word = "tweezers "
+
+sentence = word[2] + word[3] + word[-1]\
+    + word[-2] + word[2] + word[2] + word[-1]\
+    + word[0] + word[-3] + word[3] + word[3] + word[-2]
+
+print(sentence)
