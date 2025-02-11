fa451d9e1177d9de4c11796d12cb6bf5ce431000
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-11 21:34:54 +0100

    Delicious new pieces

--- 00_python-introduction/29_edibles.py
+++ 00_python-introduction/29_edibles.py
@@ -1,8 +1,10 @@
-# Extract four words of edible food items from the sentence below.
-# Use only string slicing to pick them out!
-# Feel free to use pen and paper to number the indices
-# and find the locations quicker.
-#
-# What dish can you make from these ingredients? :)
+# 0. Extract four words of edible food items from the sentence below.
+#       - "They grappled with their leggins before going to see the buttercups flourish."
+# 1. Use only string slicing to pick them out!
+# 
+# SECNARIO: Feel free to use pen and paper to number the indices
+# and find the locations quicker. What dish can you make 
+# from these ingredients? :)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 s = "They grappled with their leggins before going to see the buttercups flourish."
