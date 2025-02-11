e81cead0e2c495d332fbb3b387df4930ca566335
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-11 21:57:17 +0100

    Delicious new pieces

--- 00_python-introduction/29_edibles.py
+++ 00_python-introduction/29_edibles.py
@@ -2,9 +2,15 @@
 #       - "They grappled with their leggins before going to see the buttercups flourish."
 # 1. Use only string slicing to pick them out!
 # 
-# SECNARIO: Feel free to use pen and paper to number the indices
+# SCENARIO: Feel free to use pen and paper to number the indices
 # and find the locations quicker. What dish can you make 
 # from these ingredients? :)
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 s = "They grappled with their leggins before going to see the buttercups flourish."
+
+# making a dish of apple pie
+ingredient1 = s[7:12]
+ingredient2 = s[26:29]
+ingredient3 = s[-20:-14]
+ingredient4 = s[-9:-4]
