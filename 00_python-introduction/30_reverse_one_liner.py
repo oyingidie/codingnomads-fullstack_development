b3d97012f4c7ed82e29291093c15c604f693ff11
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-12 02:40:03 +0100

    In a pickle...

--- 00_python-introduction/30_reverse_one_liner.py
+++ 00_python-introduction/30_reverse_one_liner.py
@@ -1,4 +1,9 @@
-# Reverse the string given below in a single line of code
-# with the help of string slicing.
+# 0. Reverse the string given below in a single line of code
+#    with the help of string slicing.
+#       - palindrome = "too bad i hid a boot"
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 palindrome = "too bad i hid a boot"
+
+_reversed = palindrome[::-1]
+print(_reversed)
