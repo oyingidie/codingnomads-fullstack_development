4bedc8bfbe0c1ea4dc050cdc1c01ab52f9b15764
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-12 03:50:40 +0100

    Purple wizard with the sticky fruit juice

--- 00_python-introduction/32_occurrence.py
+++ 00_python-introduction/32_occurrence.py
@@ -1,4 +1,13 @@
-# Find the index of the first occurrence of the letter `n` in the string.
+# 0. Find the index of the first occurrence of the letter `n` in the string.
+#       - composer = "Ludvig van Beethoven"
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 composer = "Ludvig van Beethoven"
-~                                   
+
+# using the find() method
+first_occurrence = composer.find("n")
+print(first_occurrence)
+
+# using the index() method
+first_index = composer.index("n")
+print(first_index)
