eee357b0c9f58f8daab082ab02437ec2cab63efe
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-17 16:03:59 +0100

    Text is everywhere

--- 00_python-introduction/22_longest.py
+++ 00_python-introduction/22_longest.py
@@ -14,20 +14,20 @@ strong_password = "%8Ddb^ca<*'{9pur/Y(8n}^QPm3G?JJY}\(<bCGHv^FfM}.;)khpkSYTfMA@>
 
 # is the longest string in German?
 if len(longest_german_word) > len(longest_hungarian_word) \
-    and len(longest_german_word) > len(longest_finnish_word) \
-    and len(longest_german_word) > len(strong_password):
+and len(longest_german_word) > len(longest_finnish_word) \
+and len(longest_german_word) > len(strong_password):
     print("The longest string is in German with", len(longest_german_word), "characters.")
 
 # is the longest string in Hungarian?
 elif len(longest_hungarian_word) > len(longest_german_word) \
-    and len(longest_hungarian_word) > len(longest_finnish_word) \
-    and len(longest_hungarian_word) > len(strong_password):
+and len(longest_hungarian_word) > len(longest_finnish_word) \
+and len(longest_hungarian_word) > len(strong_password):
     print("The longest string is in Hungarian with", len(longest_hungarian_word), "characters.")
 
 # is the longest string in Finnish?
 elif len(longest_finnish_word) > len(longest_german_word) \
-    and len(longest_finnish_word) > len(longest_hungarian_word) \
-    and len(longest_finnish_word) > len(strong_password):
+and len(longest_finnish_word) > len(longest_hungarian_word) \
+and len(longest_finnish_word) > len(strong_password):
     print("The longest string is in Finnish with", len(longest_finnish_word), "characters.")
 
 # is the longest string the password?
