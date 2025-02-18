15997d3b89cbe8e2c0fc8e3465e3daa1dd2aca28
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-18 16:45:09 +0100

    Grocery shopping

--- 00_python-introduction/34_shorthands.py
+++ 00_python-introduction/34_shorthands.py
@@ -1,4 +1,8 @@
-# Increase the number for the `shorthands` variable by 1
-# using a shorthand assignment.
+# 0. Increase the number for the `shorthands` variable by 1
+#    using a shorthand assignment.
+#
+#               shorthands = 1
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 shorthands = 1
+shorthands += 1
