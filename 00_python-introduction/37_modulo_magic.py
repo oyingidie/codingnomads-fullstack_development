cbdf18ef3d26295b7f780f3290ba0e575dfbe8c3
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-19 11:24:12 +0100

    A common occurrence

--- 00_python-introduction/37_modulo_magic.py
+++ 00_python-introduction/37_modulo_magic.py
@@ -1,7 +1,32 @@
-# Use the modulo operator to confirm which of the values
-# shown below are divisible by seven.
+# 0. Use the modulo operator to confirm which of the values
+#    shown below are divisible by seven.
+#       - num_1 = 42
+#       - num_2 = 137
+#       - num_3 = 455
+#       - num_4 = 1997
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 num_1 = 42
 num_2 = 137
 num_3 = 455
 num_4 = 1997
+
+if (num_1 % 7) == 0:
+    print(num_1, "is divisible by seven.")
+else:
+    print(num_1, "is not divisible by seven.")
+
+if (num_2 % 7) == 0:
+    print(num_2, "is divisible by seven.")
+else:
+    print(num_2, "is not divisible by seven.")
+
+if (num_3 % 7) == 0:
+    print(num_3, "is divisible by seven.")
+else:
+    print(num_3, "is not divisible by seven.")
+
+if (num_4 % 7) == 0:
+    print(num_4, "is divisible by seven.")
+else:
+    print(num_4, "is not divisible by seven.")
