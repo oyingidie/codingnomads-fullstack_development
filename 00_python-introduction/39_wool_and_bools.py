567b21446335994a0a436eee9af734dc86f40f90
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-19 16:26:49 +0100

    Baa Baa Black Sheep

---
+++ 00_python-introduction/39_wool_and_bools.py
@@ -0,0 +1,13 @@
+# 0. Write a code snippet that gives a name to a `sheep`
+#    and uses a boolean value to define whether it has `wool` or not.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# assign name to sheep
+sheep_name = "Fluffy"
+# physical attribute of animal
+has_wool = True
+
+if has_wool:
+    print(sheep_name, "has wool.")
+else:
+    print(sheep_name, "has no wool.")
