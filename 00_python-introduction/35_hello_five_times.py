23f4307e84e891f758d7b5b415b38062612327ae
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-19 06:25:32 +0100

    In one swoop...

--- 00_python-introduction/35_hello_five_times.py
+++ 00_python-introduction/35_hello_five_times.py
@@ -1,3 +1,5 @@
 # Write the necessary code to display "Hello World!" five times.
 # TIP: Use a mathematical operator on the string!
-#
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+print("Hello World!\n" * 5)
