22fbb2f2f0047c812dee482cd75f3ee883b82f59
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-19 06:52:33 +0100

    Value to variable

--- 00_python-introduction/36_temp_converter.py
+++ 00_python-introduction/36_temp_converter.py
@@ -1,4 +1,10 @@
-# Fahrenheit to Celsius:
-# ----------------------
-# Write the necessary code to convert a degree in Fahrenheit
-# to Celsius and print it to the console. Use variable names!
+# SCENARIO: Fahrenheit to Celsius:
+# 
+# 0. Write the necessary code to convert a degree in Fahrenheit
+#    to Celsius and print it to the console. Use variable names!
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+fahrenheit = int(input("Enter the temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5.0/9.0
+
+print(fahrenheit, "degrees Fahrenheit is equal to", celsius, "degrees Celsius.")
