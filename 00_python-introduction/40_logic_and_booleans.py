3e83df86ff9f30c452e6d6779fe797f894e9bca5
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-02-20 11:09:08 +0100

    That core building block

---
+++ 00_python-introduction/40_logic_and_booleans.py
@@ -0,0 +1,14 @@
+# 0. Demonstrate the use of all logical operators (and, or, not) below.
+# 1. Create variables that hold boolean values, then combine them
+#    to express the following sentence:
+#           - do two wrongs make a right?
+#
+# 2. Note that the truth value of the statement doesn't matter,
+#    but try to use all three logical operators in one line of code
+#    to get another boolean value as your result :)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+wrong = False
+right = True
+
+print((wrong and wrong) == right)
