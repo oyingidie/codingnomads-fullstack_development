5127c4a48b62cc4d74a63745eccf8a13d80a8540
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-01 22:12:35 +0100

    Head-scratch-situation

--- 00_python-introduction/41_decypher_cipher.py
+++ 00_python-introduction/41_decypher_cipher.py
@@ -1,5 +1,15 @@
-# Decipher the message within the `secret` variable.
-# Pick out only the alphabetic characters, not the numbers.
+# 0. Decipher the message within the `secret` variable.
+# 1. Pick out only the alphabetic characters, not the numbers.
+#
+# code >>>  secret = "2349h30023388281e3299371l1l3094842o0333322883"
+#           solution = ""   >>>
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 secret = "2349h30023388281e3299371l1l3094842o0333322883"
 solution = ""
+
+for char in secret:
+    if char.isalpha:
+        solution += char
+
+print(solution)
