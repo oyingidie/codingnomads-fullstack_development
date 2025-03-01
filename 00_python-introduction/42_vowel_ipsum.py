f9b4783ad2cc15a446c2b3225a867cefc8742a5c
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-01 23:24:54 +0100

    Everything is an object

--- 00_python-introduction/42_vowel_ipsum.py
+++ 00_python-introduction/42_vowel_ipsum.py
@@ -1,7 +1,6 @@
 # 0. Print the total number of vowels that are used in the lorem ipsum text.
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-vowel_ipsum = ""
 lorem_ipsum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit,
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -10,3 +9,10 @@ voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
 mollit anim id est laborum."""
 
+vowel_ipsum = ""
+
+for char in lorem_ipsum:
+    if char.lower() in ('a', 'e', 'i', 'o', 'u'):
+        vowel_ipsum += char
+
+print(vowel_ipsum)
