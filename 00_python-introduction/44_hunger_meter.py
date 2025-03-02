55d040f47cbd60a747e15eca2e7812200c8e1b95
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-02 22:32:05 +0100

    Give it a spin

--- 00_python-introduction/44_hunger_meter.py
+++ 00_python-introduction/44_hunger_meter.py
@@ -1,6 +1,7 @@
 # SCENARIO: Your hunger-meter currently only handles string input accurately.
-# Replace your first `if` statement with a type check.
-# If the value of `hunger` is not of the type `str`,
+#
+# 0. Replace your first `if` statement with a type check.
+# 1. If the value of `hunger` is not of the type `str`,
 # print a message that reminds you to
 # declare your hunger levels with a string.
 
