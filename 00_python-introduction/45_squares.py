213f6d52b35c7d19900e70292b05c3eba3d40098
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-03 16:52:36 +0100

    Top to bottom, or maybe not

--- 00_python-introduction/45_squares.py
+++ 00_python-introduction/45_squares.py
@@ -1,3 +1,6 @@
 # 0. Write a script that prints out all the squares of numbers from 1 to 50.
 # 1. Use a `for` loop that demonstrates the use of the `range()` function.
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+for n in range(1, 51):
+    print(n ** 2)
