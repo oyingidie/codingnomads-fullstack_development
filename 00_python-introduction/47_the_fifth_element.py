a2bdca603d7f883835fc5a4fc24be7399e0ceb4b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-04 09:17:01 +0100

    Immutable sequence of numbers

--- 00_python-introduction/47_the_fifth_element.py
+++ 00_python-introduction/47_the_fifth_element.py
@@ -1,2 +1,3 @@
 # 0. Use a `for` loop to print out every fifth number counting from 1 to 1000.
 # i.e. sum 5, 10, 15, 20 ...
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
