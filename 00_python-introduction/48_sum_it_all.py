b2733d93ede9c63a65a857d25ee66964e7101ad4
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-04 16:47:46 +0100

    Order matters...

--- 00_python-introduction/48_sum_it_all.py
+++ 00_python-introduction/48_sum_it_all.py
@@ -1,8 +1,8 @@
-# Using a loop, sum all numbers from the `start` to the `stop` number.
-# The sequence should consist only of integers from 1 to 100.
-# The output of your calculation should look like this:
+# 0. Using a loop, sum all numbers from the `start` to the `stop` number.
+# 1. The sequence should consist only of integers from 1 to 100.
+# 2. The output of your calculation should look like this:
+#       - The sum is: 5050
 #
-#      The sum is: 5050
-
-start = 1
-stop = 100
+# code >>>  start = 1
+#           stop = 100  >>>
+#
