ddacc3ca554451fb472e114182b86804acb8da9f
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-22 15:00:39 +0100

    Ops in the loop

--- 00_python-introduction/49_primes.py
+++ 00_python-introduction/49_primes.py
@@ -1,10 +1,12 @@
 # 0. Print out every prime number between 1 and 1000.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# define range
 start = 1
 stop = 1000
 is_prime = False
 
+# loop through the range
 for num in range(start, stop + 1):
     if num > start:
         is_prime = True
@@ -14,5 +16,6 @@ for num in range(start, stop + 1):
                     is_prime = False
                     break
         
+        # print prime numbers
         if is_prime:
             print(num)
