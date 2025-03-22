4d5d59ae23a5ec798cdd496c9ca198205bea4d13
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-22 14:51:47 +0100

    Behind the scenes

--- 00_python-introduction/50_range_table.py
+++ 00_python-introduction/50_range_table.py
@@ -10,8 +10,8 @@
 
 # loop from 0 to 50 in steps of 10
 for num in range(0, 50, 10):
-    # loop from first to last number in each step
+    # loop through the range of numbers in each step
     for i in range(num, num + 10):
-        # print 
+        # print each number
         print(i, end=" ")
     
