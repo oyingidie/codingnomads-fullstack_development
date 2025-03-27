2473a1fe799f21c896e295dd39fef37327f170b9
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-27 19:42:50 +0100

    A bite of python

--- 00_python-introduction/51_fizzbuzz.py
+++ 00_python-introduction/51_fizzbuzz.py
@@ -6,3 +6,5 @@
 #       - If the number is divisible by 5, write `Buzz` instead of the number
 #       - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+for num in range(1, n + 1):
