bb296ccd753c07e54d136656bd62f28be7a27689
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-27 19:39:29 +0100

    A bite of python

--- 00_python-introduction/51_fizzbuzz.py
+++ 00_python-introduction/51_fizzbuzz.py
@@ -5,4 +5,4 @@
 #       - If the number is divisible by 3, write `Fizz` instead of the number
 #       - If the number is divisible by 5, write `Buzz` instead of the number
 #       - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
-# ++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
