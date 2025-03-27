20589631be8f3e44c9ee21fea9bb5399bb917115
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-27 19:39:02 +0100

    A bite of python

--- 00_python-introduction/51_fizzbuzz.py
+++ 00_python-introduction/51_fizzbuzz.py
@@ -1,7 +1,8 @@
 # 0. You start this journey with a number `n`.
 # 1. You have to display a string representation of all numbers from 1 to n,
 #    but there are some constraints:
+#
 #       - If the number is divisible by 3, write `Fizz` instead of the number
 #       - If the number is divisible by 5, write `Buzz` instead of the number
 #       - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
-# 
+# ++++++
