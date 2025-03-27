638d8ad3f44d82630fc352e50699fd0419766a61
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-27 20:11:31 +0100

    A bite of python

--- 00_python-introduction/51_fizzbuzz.py
+++ 00_python-introduction/51_fizzbuzz.py
@@ -7,6 +7,8 @@
 #       - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+n = int(input("Set a number limit:"))
+
 for num in range(1, n + 1):
     if (num % 3 == 0) and (num % 5 == 0):
         print("FizzBuzz")
