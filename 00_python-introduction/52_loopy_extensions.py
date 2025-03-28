c9ca6f28cd0f80d0bace335ea3d03d8e1aae2327
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-28 18:29:55 +0100

    Parsing for a human reader

--- 00_python-introduction/52_loopy_extensions.py
+++ 00_python-introduction/52_loopy_extensions.py
@@ -2,7 +2,7 @@
 #           - filename = "operators.pdf"
 #
 # 1: Don't use the string method you've used to solve this before!
-#Don't use the `in` keyword to look for a sub-string!
+# 2: Don't use the `in` keyword to look for a sub-string!
 #Don't use any string slicing technique either!
 #
 # You'll see that it'll be tricky to solve this challenge with a loop :)
