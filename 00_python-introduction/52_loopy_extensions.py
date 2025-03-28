b5b47705ab85849281097c6bcfa0dbbf1dd25110
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-28 18:30:13 +0100

    Parsing for a human reader

--- 00_python-introduction/52_loopy_extensions.py
+++ 00_python-introduction/52_loopy_extensions.py
@@ -3,7 +3,7 @@
 #
 # 1: Don't use the string method you've used to solve this before!
 # 2: Don't use the `in` keyword to look for a sub-string!
-#Don't use any string slicing technique either!
+# 3: Don't use any string slicing technique either!
 #
 # You'll see that it'll be tricky to solve this challenge with a loop :)
 # Remember to use also other techniques you've learned,
