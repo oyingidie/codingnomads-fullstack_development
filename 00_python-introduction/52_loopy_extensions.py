3543b70957135201f44fadb8f5ef4ca9c32dc422
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-03-28 18:31:07 +0100

    Parsing for a human reader

--- 00_python-introduction/52_loopy_extensions.py
+++ 00_python-introduction/52_loopy_extensions.py
@@ -8,5 +8,6 @@
 # You'll see that it'll be tricky to solve this challenge with a loop :)
 # Remember to use also other techniques you've learned,
 # for example flags and conditional statements.
+# 
 
 filename = "operators.pdf"
