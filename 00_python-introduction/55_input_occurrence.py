4033b53e260bc536dae1745a245565f5f311744b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-18 21:22:59 +0100

    Ah! I get it…

--- 00_python-introduction/55_input_occurrence.py
+++ 00_python-introduction/55_input_occurrence.py
@@ -1,6 +1,8 @@
 # 0. Write a script that takes a string of words and a letter from the user.
-# 1. Find the index of first occurrence of the letter in the string. For example:
+# 1. Find the index of first occurrence of the letter in the string.
+#    Example:
 #
 #           String input: hello world
 #           Letter input: o
 #           Result: 4
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
