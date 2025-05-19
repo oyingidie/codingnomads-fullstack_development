3b2b59b13915887c9012216515f98914bc605f54
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-19 21:56:52 +0100

    Communicate with the script

--- 00_python-introduction/56_input_replace.py
+++ 00_python-introduction/56_input_replace.py
@@ -1,6 +1,7 @@
 # 0. Write a script that takes a string of words and a symbol from the user.
-# 1. Replace all occurrences of the first letter with the symbol. For example:
+# 1. Replace all occurrences of the first letter with the symbol.
+#    Example:
 #
-# String input: more python programming please
-# Symbol input: §
-# Result: §ore python progra§§ing please
+#           String input: more python programming please
+#           Symbol input: §
+#           Result: §ore python progra§§ing please
