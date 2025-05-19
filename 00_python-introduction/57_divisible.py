9e03e5be7de6b506b84d85ee7b0544775a04691f
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-19 23:15:58 +0100

    Patiently, Python waits.

--- 00_python-introduction/57_divisible.py
+++ 00_python-introduction/57_divisible.py
@@ -1,3 +1,4 @@
 # 0. Write a program that takes a number between 1 and 1,000,000,000 from the user
 #    and determines whether it is divisible by 3 using an `if` statement.
 # 1. Print the result.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
