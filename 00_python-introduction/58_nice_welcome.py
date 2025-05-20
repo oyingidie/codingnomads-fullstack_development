2246b67cb8ca322382fc83e457be7fb8bdaa5b67
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-20 22:40:43 +0100

    Hooks, triggers, and more...

--- 00_python-introduction/58_nice_welcome.py
+++ 00_python-introduction/58_nice_welcome.py
@@ -1,6 +1,5 @@
 # 0. Ask the user to input their name.
-# 1. Print a nice welcome message
-# that welcomes them personally to your script.
-# If a user enters more than one name, e.g. "firstname lastname",
-# then use only their first name to overstep some personal boundaries
-# in your welcome message.
+# 1. Print a nice welcome message that welcomes them personally to your script.
+# 2. If a user enters more than one name, e.g. "firstname lastname",
+#    then use only their first name to overstep some personal boundaries
+#    in your welcome message.
