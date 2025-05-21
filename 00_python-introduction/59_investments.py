0a828c73450333928d82b31955f86ef57462454c
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-21 23:10:15 +0100

    Charterholder

--- 00_python-introduction/59_investments.py
+++ 00_python-introduction/59_investments.py
@@ -6,10 +6,12 @@
 # 1. Calculate the future values and print them to the console.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+print(" >>> Investment Calculator <<< \n")
+
 amount = float(input("Enter your investment amount: "))
 interest_rate = float(input("Enter your preferred interest rate in percentage: "))
 period = int(input("Enter the number of years you wish to invest for: "))
 
 future_value = amount * (1 + interest_rate / 100) ** period
 
-print(f"Your investment of {amount} will be worth {future_value:.2f} in {period} years time.")
+print(f"At {interest_rate}%, your investment of {amount} will be worth {future_value:.2f} in {period} years time.")
