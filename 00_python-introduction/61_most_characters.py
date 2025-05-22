ccd0f26b9d1ccab0011782fcb97479994a1e885a
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-22 14:54:48 +0100

    Flipping Statements & Embedded Expressions

--- 00_python-introduction/61_most_characters.py
+++ 00_python-introduction/61_most_characters.py
@@ -16,4 +16,10 @@ first_string = input("The first string: ")
 second_string = input("The second string: ")
 third_string = input("The third string: ")
 
+length_0 = len(first_string)
+length_1 = len(second_string)
+length_2 = len(third_string)
 
+if 
+len(first_string) > len(second_string) and len(first_string) > len(third_string):
+    print(f"\"{first_string}\" is the longest string with {len(first_string)} characters")
