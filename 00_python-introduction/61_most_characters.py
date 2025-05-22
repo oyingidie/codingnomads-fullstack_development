7bb34ca623c46c25ac10f34945b19a528c640217
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-22 13:01:34 +0100

    Flipping Statements & Embedded Expressions

--- 00_python-introduction/61_most_characters.py
+++ 00_python-introduction/61_most_characters.py
@@ -1,10 +1,11 @@
-# Write a script that takes three strings from the user
-# and prints the longest string together with its length.
+# 0. Write a script that takes three strings from the user
+#    and prints the longest string together with its length.
 #
-# Example Input:
-#     hello
-#     world
-#     greetings
+#       Example [Input]:
+#                   -   hello
+#                   -   world
+#                   -   greetings
 #
-# Example Output:
-#     9, greetings
+#       Example [Output]:
+#                   -   9, greetings
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
