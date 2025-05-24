63dac88b7a06b1f309350cb0e9a61e78cafda73b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-24 09:23:00 +0100

    Flipping Statements & Embedded Expressions

--- 00_python-introduction/61_most_characters.py
+++ 00_python-introduction/61_most_characters.py
@@ -35,6 +35,8 @@ while length_0 == 0 or length_1 == 0 or length_2 == 0:
         print("Both your first and second strings are empty. Please enter a string for each.")
         first_string = input("Your first string: ")
         second_string = input("Your second string: ")
+        length_0 = len(first_string)
+        length_1 = len(second_string)
 
 if (length_0 > length_1) and (length_0 > length_2):
     print(f"\"{first_string}\" is the longest string with {length_0} characters!")
