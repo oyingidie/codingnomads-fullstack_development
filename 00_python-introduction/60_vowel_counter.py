75a520df339f347ed60938d65885802040321667
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-24 15:25:15 +0100

    Loopy, loopy, loopy...

--- 00_python-introduction/60_vowel_counter.py
+++ 00_python-introduction/60_vowel_counter.py
@@ -1,9 +1,9 @@
 # 0. Write a script that takes a string input from a user and prints
 #    a total count of how often each individual vowel appeared.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-pseudoname = "\N{mathematical bold capital d}"\
+control = "\N{mathematical bold capital d}"\
 
-print("Hello! My name is Detective... I'm on the lookout for vowels in your data.")
+print(f"Hello! My name is {control}... I'm on the lookout for vowels in your data.")
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 user_input = input("Enter any data of your choice: ")
