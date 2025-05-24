c3b0bb7a52102ee45e648f04cfe93b4aa449ba1d
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-24 09:28:54 +0100

    Flipping Statements & Embedded Expressions

--- 00_python-introduction/61_most_characters.py
+++ 00_python-introduction/61_most_characters.py
@@ -34,7 +34,8 @@ while length_0 == 0 or length_1 == 0 or length_2 == 0:
         third_string = input("Your third string: ")
         length_0 = len(first_string)
         length_2 = len(third_string)
-
+    elif (length_1 ==0) and (length_2 == 0):
+        print("Both your second and third strings are empty. Please enter a string for each.")
     elif length_0 == 0:
         first_string = input("Your first string is empty.\nPlease enter a string: ")
         length_0 = len(first_string)
