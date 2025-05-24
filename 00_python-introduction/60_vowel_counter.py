f0f23f72d9fca8034e7f451258a6c9bfa78cc7ba
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-24 14:46:44 +0100

    Loopy, loopy, loopy…

--- 00_python-introduction/60_vowel_counter.py
+++ 00_python-introduction/60_vowel_counter.py
@@ -3,15 +3,15 @@
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 vowels = ['a', 'e', 'i', 'o', 'u']
-user_input = input("Enter any 'thing' of your choice: "
+user_input = input("Enter any data of your choice: "
 
 for vowel in vowels:
     vowel_count = 0
     vowel_count += user_input.lower().count(vowel)
 
     if vowel_count == 0:
-        print(f"The text does not contain the vowel '{vowel}'.")
+        print(f"Your data does not contain the vowel '{vowel}'.")
     elif vowel_count == 1:
-        print(f"The vowel '{vowel}' appeared only once in the text.")
+        print(f"The vowel '{vowel}' appeared only once in your data.")
     else:
-        print(f"The vowel '{vowel}' appeared {vowel_count} times in the text.")
+        print(f"The vowel '{vowel}' appeared {vowel_count} times in your data.")
