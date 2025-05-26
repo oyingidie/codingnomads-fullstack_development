d6a309e69e46e21e30ddace59c2952c620a26173
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-26 21:42:08 +0100

    Conversion is user dependent

--- 00_python-introduction/62_month_reminder.py
+++ 00_python-introduction/62_month_reminder.py
@@ -14,4 +14,13 @@ if 1 <= month_number <= 12:
         print(f"February is the {month_number}nd month of the year.")
     elif month_number == 3:
         print(f"March is the {month_number}rd month of the year.")
-    
+    elif month_number == 4:
+        print(f"April is the {month_number}th month of the year.")
+    elif month_number == 5:
+        print(f"May is the {month_number}th month of the year.")
+    elif month_number == 6:
+        print(f"June is the {month_number}th month of the year.")
+    elif month_number == 7:
+        print(f"July is the {month_number}th month of the year.")
+    elif month_number == 8:
+        print(f"August is the {month_number}th month of the year.")
