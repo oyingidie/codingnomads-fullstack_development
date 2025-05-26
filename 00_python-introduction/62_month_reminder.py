4e6e23355826335112063c3f5d7d0ae8cc0658e5
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-26 20:58:13 +0100

    Conversion is user dependent

--- 00_python-introduction/62_month_reminder.py
+++ 00_python-introduction/62_month_reminder.py
@@ -1,5 +1,6 @@
-# Take in a number between 1 and 12 from the user
-# and print the name of the associated month:
-# "January", "February", ... "December"
-# Print "Error" if the number from the user is not between 1 and 12.
-# Use a nested `if` statement.
+# 0. Take in a number between 1 and 12 from the user
+#    and print the name of the associated month:
+#               "January", "February", ... "December"
+# 1. Print "Error" if the number from the user is not between 1 and 12.
+# SCENARIO: Use a nested `if` statement.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
