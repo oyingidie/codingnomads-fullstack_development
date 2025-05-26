bbd263e35b91a54a66723dd32d56c68b58e26e81
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-26 22:02:50 +0100

    Let them give it a spin

--- 00_python-introduction/63_number_search.py
+++ 00_python-introduction/63_number_search.py
@@ -1,7 +1,10 @@
 # 0. Ask your user for a number between 0 and 1,000,000,000.
-# 1. Use a `while` loop to find the number. When the number is found,
-#    exit the loop and print the number to the console.
+# 1. Use a `while` loop to find the number.
+# 
+# SCENARIO: When the number is found, exit the loop and 
+#print the number to the console.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 number = int(input("Enter a number between 0 and 1,000,000,000: "))
 
+while True:
