bf5b148dfc7da8e595fa2bbb7b680fdf10924434
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-28 14:58:18 +0100

    Open up the gates!

--- 00_python-introduction/64_sarcastic_prompt.py
+++ 00_python-introduction/64_sarcastic_prompt.py
@@ -18,12 +18,4 @@ for char in user_opinion:
     else:
         sarcastic_response += char
 
-
-for char in prompt:
-    char_index = prompt.index(char)
-    if char_index % 2 == 0:
-        sarcastic_response += char.lower()
-    else:
-        sarcastic_response += char.upper()
-
 print(f"\nWow! your opinion is so unique. It really stands out:\n\"{sarcastic_response}\"")
