0aedff58de11cca6508712fefd1ba6834386d408
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-28 04:57:20 +0100

    Open up the gates!

--- 00_python-introduction/64_sarcastic_prompt.py
+++ 00_python-introduction/64_sarcastic_prompt.py
@@ -1,3 +1,14 @@
 # 0. Create a sarcastic program that asks a user for their honest opinion,
 #    then prints the same sentence back to them in aLtErNaTiNg CaPs.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+prompt = input("Please share your honest opinion: ")
+sarcastic_response = ""
+
+for char in prompt:
+    char_index = prompt.index(char)
+    if char_index % 2 == 0:
+        sarcastic_response += char.lower()
+    else:
+        sarcastic_response += char.upper()
+
