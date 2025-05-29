aa70422365d5f1c2d07deaa02c80371802eb223b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-05-29 08:19:34 +0100

    A well-deserved lunchtime break

--- 00_python-introduction/65_cococoding.py
+++ 00_python-introduction/65_cococoding.py
@@ -1,4 +1,4 @@
 
 # 0. Write the necessary code to display the following message to the console:
 #        Time for co-co-co-ding.
-# Use an operator and f-string formatting to create this output
+# SCENARIO: Use an operator and f-string formatting to create this output
