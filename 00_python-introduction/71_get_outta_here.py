6cfe15d7551223969611c6aba43939835ca45816
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 07:18:37 +0100

    You won’t be going out tonight!

--- 00_python-introduction/71_get_outta_here.py
+++ 00_python-introduction/71_get_outta_here.py
@@ -1,5 +1,7 @@
-# Use the built-in `sys` module to explicitly quit your script.
-# Include this functionality into a loop where you're asking the user
-# for input in an infinite `while` loop.
-# If the user enters the word "quit", you can exit the program
+# 0. Use the built-in `sys` module to explicitly quit your script.
+# 1. Include this functionality into a loop where you're asking the user
+#    for input in an infinite `while` loop.
+#
+# SCENARIO: If the user enters the word "quit", you can exit the program
 # using a functionality provided by this module.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
