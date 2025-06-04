ce7a03683f1e1483b235fbb49b8ede62c1735e1b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 14:07:30 +0100

    Still only dreaming

--- 00_python-introduction/72_guess_again.py
+++ 00_python-introduction/72_guess_again.py
@@ -5,8 +5,13 @@
 
 import random
 
-number = random.randint(1, 50)
+number = random.randint(1, 100)
 attempts = 5
-print("It's the Guess-My-Number game. Let's play!")
+print("It's the Guess-My-Number game. Let's play!\n")
 
 while attempts > 0:
+    print(f"Attempts left: {attempts}")
+    guess = int(input("Guess a number between 1 and 100: "))
+
+    if guess == number:
+        print("Congrats! \U0001F389 Your guess is correct! \U0001F973")
