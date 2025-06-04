e3aa74a5ab16b79bc7b52ca97a442a414b5e1f2a
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 04:39:42 +0100

    “Python comes with a lot of batteries.”

--- 00_python-introduction/69_the_time_please.py
+++ 00_python-introduction/69_the_time_please.py
@@ -1,2 +1,3 @@
-# Use a built-in Python module to tell you the current date and time.
-# Research online, so you can print it in a readable manner.
+# 0. Use a built-in Python module to tell you the current date and time.
+# 1. Research online, so you can print it in a readable manner.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
