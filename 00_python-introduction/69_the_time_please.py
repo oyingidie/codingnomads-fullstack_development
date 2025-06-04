9ee1c48096ea02ca49ee5308a9d9d0c1d21d4a58
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 05:28:36 +0100

    “Python comes with a lot of batteries.”

--- 00_python-introduction/69_the_time_please.py
+++ 00_python-introduction/69_the_time_please.py
@@ -6,5 +6,7 @@ import datetime
 
 current_date_time = datetime.datetime.now()
 formatted_date = current_date_time.strftime("%A, %d %B %Y")
-formatted_time = current_date_time.strftime("%H:%M:%S")
+formatted_time = current_date_time.strftime("%I:%M %p")
 
+print(f"In case you were wondering, it is currently {formatted_time} on {formatted_date}.")
+print("Make the most of it!\nRemember, time is a precious resource.")
