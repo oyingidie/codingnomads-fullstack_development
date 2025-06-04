61f75e3d7d7ba2de94a62231599dd5df839139a2
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 13:49:14 +0100

    Your friendly neighbourhood placeholder

--- 00_python-introduction/5_fix_missing_pass.py
+++ 00_python-introduction/5_fix_missing_pass.py
@@ -1,5 +1,11 @@
 # SCENARIO: The code below runs into an error.
 #
+# code >>>  flag = False
+#           while flag:
+#               # keep doing what you're doing
+#
+#           print("all done.") >>>
+#
 # 0. Fix it, without making Python do anything.
 # +++++++++++++++++++++++++++++++++++++++++++++
 
