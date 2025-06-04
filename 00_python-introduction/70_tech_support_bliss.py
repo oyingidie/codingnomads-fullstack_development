621b9e779dc2f396d377f9e016da34390bea3aeb
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 06:59:44 +0100

    It’s a colorful image

--- 00_python-introduction/70_tech_support_bliss.py
+++ 00_python-introduction/70_tech_support_bliss.py
@@ -15,3 +15,6 @@ import platform
 print(f"{'Platform:':>10} {platform.platform()}")
 print(f"{'Compiler:':>10} {platform.python_compiler()}")
 print(f"{'Python:':>10} {platform.python_version()}")
+print(f"{'System:':>10} {platform.system()}")
+print(f"{'Release:':>10} {platform.release()}")
+print(f"{'Processor:':>10} {platform.processor()}")
