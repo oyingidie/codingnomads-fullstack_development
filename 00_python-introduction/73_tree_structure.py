e2c56d29b2fea71a811866a62bdcb1a90d7cb6bd
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 17:09:27 +0100

    Ah! Fresh air!

--- 00_python-introduction/73_tree_structure.py
+++ 00_python-introduction/73_tree_structure.py
@@ -1,3 +1,5 @@
-# Write a script that walks through a nested folder structure
-# and prints out all the Python files it can find.
-# Run it in your labs folder and add formatting for nicer viewing.
+# 0. Write a script that walks through a nested folder structure
+#    and prints out all the Python files it can find.
+# 1. Run it in your labs folder and add formatting for nicer viewing.
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
