7b443a3504c2629355f47be3d1afbfd1c0ee1748
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 04:32:55 +0100

    On the shoulders of giants

--- 00_python-introduction/68_math_pi.py
+++ 00_python-introduction/68_math_pi.py
@@ -1,2 +1,7 @@
 # 0. Access and print the value of pi from Python's `math` module.
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import math
+
+pi_value = math.pi
+print(f"The value of pi is: {pi_value}")
