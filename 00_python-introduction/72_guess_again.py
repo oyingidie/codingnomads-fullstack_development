cbea30aff63907f7ffa6794b0b71c05b73471d1c
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 13:59:57 +0100

    Still only dreaming

--- 00_python-introduction/72_guess_again.py
+++ 00_python-introduction/72_guess_again.py
@@ -2,3 +2,11 @@
 # This time, give your players only a certain amount of tries
 # before they lose.
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import random
+
+number = random.randint(1, 50)
+attempts = 5
+print("It's the Guess-My-Number game. Let's play!")
+
+while attempts > 0:
