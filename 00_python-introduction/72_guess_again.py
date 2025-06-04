03aa663cf6b9abdd03380dd1f1ecaa0c060858ad
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-06-04 15:27:39 +0100

    Still only dreaming

--- 00_python-introduction/72_guess_again.py
+++ 00_python-introduction/72_guess_again.py
@@ -7,11 +7,18 @@ import random
 
 number = random.randint(1, 100)
 attempts = 5
-print("It's the Guess-My-Number game. Let's play!\n")
+print("It's the Guess-My-Number game. Let's play!")
 
 while attempts > 0:
-    print(f"Attempts left: {attempts}")
+    print(f"\nAttempts left: {attempts}")
     guess = int(input("Guess a number between 1 and 100: "))
 
     if guess == number:
-        print("Congrats! \U0001F389 Your guess is correct! \U0001F973")
+        print("\nCongrats! \U0001F389\nYour guess is correct! \U0001F973")
+        break
+    elif guess < number:
+        print("\nYour guess is too low...")
+        attempts -= 1
+    elif guess > number:
+        print("\nYour guess is too high...")
+        attempts -= 1
